d3b7e46f269a5bbd42f9379cb3968fd416ae0e78
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-08-11 14:16:25 +0200

    Header and card flex structure built

---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+import '../Stylesheets/Card.css';
+
+function Card() {
+  return (
+  <div className="card">
+  Card
+  </div>
+  );
+}
+
+export default Card;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import '../Stylesheets/Header.css';
+
+function Header() {
+  return (
+  <div className="header">
+    <div>logo</div>
+    <div>search</div>
+    <div>profile</div>
+  </div>
+  );
+}
+
+export default Header;
